1e29bbbcd8c1cd0b2554b9b84a76167665a11552
Suriya-Panneerselvam <suriyapanneerselvam12015@gmail.com>
2025-09-02 14:33:47 +0530

    updation

--- Problem 2/ATM.java
+++ Problem 2/ATM.java
@@ -2,47 +2,54 @@ import java.util.*;
 
 public class ATM {
     static class Account {
-        int accountno=6201;
-        int amount=10000;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        {
+            hm.put(6201, 10000);
+            hm.put(6202, 5800);
+            hm.put(6203, 12000);
+        }
     }
 
     static class Balance extends Account{
-        void display(){
-            System.out.println("Account number:"+ accountno );
-            System.out.println("Balance:"+ amount );
+        void display(int an){
+            if (hm.containsKey(an) ){
+                System.out.println("Account number:"+ an );
+                System.out.println("Balance:"+ hm.get(an) );
+            }
+            else{
+                System.out.println("Invalid account number");
+            }
         }
     }
 
     static class Deposit extends Account{
         void deposit(int an,int a){
-            for (int i=0;i<3;i++){
-                if (accountno==an ){
+                if (hm.containsKey(an) ){
+                    int amount = hm.get(an);
                     amount+=a;
+                    hm.put(an, amount);
                     System.out.println("Deposit amount:"+ a);
                     System.out.println("Balance:"+ amount );
-                    break;
                 }
                 else{
                     System.out.println("Invalid account number or pin number");
-                }
-            }
-        
+                }        
         }
     } 
 
     static class Withdraw extends Account{
         void withdraw(int an,int a){
-            for (int i=0;i<3;i++){
+            int amount = hm.get(an);
+            
                 if ( amount>=a){
                     amount-=a;
+                    hm.put(an, amount);
                     System.out.println("Withdraw amount:"+ a);
                     System.out.println("Balance:"+ amount );
-                    break;
                 }
                 else{
                     System.out.println("Invalid account number or insufficient balance" );
                 }
-            }
         }
     }
 
@@ -53,36 +60,42 @@ public class ATM {
         System.out.println("Enter account no");
         int an =s.nextInt();
         Account acc=new Account();
-        if (acc.accountno==an){
-            System.out.println("selection options: BALANCE-1, DEPOSIT-2,WITHDRAW-3");
-        int option = s.nextInt();
+        if (acc.hm.containsKey(an)){
+            while (true){
+                System.out.println("selection options: BALANCE-1, DEPOSIT-2,WITHDRAW-3");
+                int option = s.nextInt();
 
-            switch (option) {
-                case 1:
-                    Balance b = new Balance();
-                    b.display();
+                switch (option) {
+                    case 1:
+                        Balance b = new Balance();
+                        b.display(an);
+                        break;
+                    case 2:
+                        Deposit d = new Deposit();
+                        System.out.println("Enter amount");
+                        int a = s.nextInt();
+                        d.deposit(an, a);
+                        break;
+                    case 3:
+                        Withdraw w = new Withdraw();
+                        System.out.println("Enter amount");
+                        int ai = s.nextInt();
+                        w.withdraw(an, ai);
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+
+                System.out.println("Do you want to continue? (yes/no)");
+                String ch = s.next().toLowerCase();
+                if (ch.equals("no")){
                     break;
-                case 2:
-                    Deposit d = new Deposit();
-                    System.out.println("Enter amount");
-                    int a = s.nextInt();
-                    d.deposit(an, a);
-                    break;
-                case 3:
-                    Withdraw w = new Withdraw();
-                    System.out.println("Enter amount");
-                    int ai = s.nextInt();
-                    w.withdraw(an, ai);
-                    break;
-        
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
             }
         }
         else{
             System.out.println("Invalid account number");
-
         }
         s.close();
     }
